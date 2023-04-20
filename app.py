32218b85279fe991baef5d4dc652c8fe24a64c0f
Jussi Pohjolainen <jussi.pohjolainen@gmail.com>
2023-04-20 15:24:43 +0300

    Add dropbox integration

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from flask import Flask
+import dropbox
+from flask import Flask, request, jsonify, json
+
+# hae renderistä access token
+access_token = os.getenv("avain")
+dbx = dropbox.Dropbox(access_token)
 
 app = Flask(__name__)
 
-@app.route("/")
-def root():
-    return "<h1>hello</h1>"
+@app.route('/customers', methods=['POST'])
+def add_customer():
+    customer = request.get_json()
+    customer_json = json.dumps(customer)
 
-if __name__ == "__main__":
+    # tallenna saatu data dropboxiin!
+    dbx.files_upload(customer_json.encode("utf-8"), '/highscore.txt')
+    return jsonify({'message': 'Customer added successfully!'})
+
+if __name__ == '__main__':
     app.run()
-
