c419a8b624782391ad40d6a3d4cc30454d275994
Jussi Pohjolainen <jussi.pohjolainen@gmail.com>
2023-04-24 13:11:37 +0300

    add oauth2

--- app.py
+++ app.py
@@ -4,55 +4,77 @@ import dropbox
 from flask import Flask, request, jsonify, json
 import requests
 
-# hae renderistä access token
+app = Flask(__name__)
+
 access_token = os.getenv("avain")
 
 dbx = dropbox.Dropbox(access_token)
 
-app = Flask(__name__)
 
 CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
-REDIRECT_URI = "https://mun-super-palvelu-jee-jee.onrender.com/callback"
-TOKEN_URL = "https://api.dropboxapi.com/oauth2/token"
 
-@app.route('/callback', methods=['GET'])
-def callback():
+AUTH_URL = 'https://www.dropbox.com/oauth2/authorize'
+TOKEN_URL = 'https://api.dropbox.com/oauth2/token'
+REDIRECT_URI = 'http://localhost:5000/auth'
+
+auth_flow = dropbox.DropboxOAuth2FlowNoRedirect(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, 'token')
+
+# Location in Dropbox to store the access token file
+ACCESS_TOKEN_FILE_PATH = '/access_token.txt'
+
+@app.route('/auth')
+def auth():
+    # Get the authorization code from the URL query parameters
     code = request.args.get('code')
 
-    if code:
-        payload = {
-            'code': code,
-            'grant_type': 'authorization_code',
-            'client_id': CLIENT_ID,
-            'client_secret': CLIENT_SECRET,
-            'redirect_uri': REDIRECT_URI
-        }
-        
-        response = requests.post(TOKEN_URL, data=payload)
-        if response.status_code == 200:
-            token_data = response.json()
-            access_token = token_data['access_token']
-            refresh_token = token_data['refresh_token']
-            
-            # Save the access token and refresh token securely (e.g. in a database or encrypted file)
-            # Use the access token to make API requests and refresh it when it expires
+    # Use the authorization code to obtain a new access token
+    try:
+        access_token, user_id = auth_flow.finish(code)
+        # Store the new access token securely
+        # ...
+        return redirect(url_for('get_customers'))
+    except dropbox.exceptions.AuthError as e:
+        return jsonify({'error': str(e)})
 
-            return f"Access token: {access_token}, Refresh token: {refresh_token}"
-        else:
-            return f"Error: {response.text}"
-    else:
-        error = request.args.get('error', 'No error information provided')
-        return f"Error during authorization: {error}"
+@app.route('/auth/start')
+def start_auth():
+    auth_url = auth_flow.start()
+    return redirect(auth_url)
+
+
+def load_access_token():
+    # Download the access token file from Dropbox
+    try:
+        metadata, res = dbx.files_download(ACCESS_TOKEN_FILE_PATH)
+        access_token = res.content.decode('utf-8').strip()
+        return access_token
+    except dropbox.exceptions.ApiError as e:
+        print('Error downloading access token file:', e)
+        raise e
+
 
 @app.route('/customers', methods=['GET'])
 def get_customers():
+    # Load the access token from the text file in Dropbox
+    
+    access_token = load_access_token()
+
+    dbx = dropbox.Dropbox(access_token)
+
+
+
     metadata, res = dbx.files_download('/highscores.json')
     data = json.loads(res.content.decode("utf-8"))
     return jsonify(data)
 
 @app.route('/customers', methods=['POST'])
 def add_customer():
+    # Load the access token from the text file in Dropbox
+    access_token = load_access_token()
+
+    dbx = dropbox.Dropbox(access_token)
+
     customer = request.get_json()
     customer_json = json.dumps(customer)
 
